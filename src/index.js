ddc3b02cd317e952a79dd49ed4e9adee91074401
alexandraBouillon <alexandra.bouillon@gmail.com>
2021-09-01 04:42:53 -0700

    Adding a User model

--- src/index.js
+++ src/index.js
@@ -1,62 +1,46 @@
+const { PrismaClient } = require('@prisma/client')
 const { ApolloServer } = require('apollo-server');
+const fs = require('fs');
+// import { readFileSync } from 'fs';
+const  readFileSync =  fs.readFileSync()
 
-// Defining a new Link type that represents the links that can be posted to Hacker News.
-// Each Link has an id, a description, and a url.
-//  it’s not needed any more because the schema definition now lives in its own file(schema.graphql)
-// const typeDefs = `
-//   type Query {
-//     info: String!
-//     feed: [Link!]!
-//   }
-
-//   type Mutation {
-//     post(url: String!, description: String!): Link!
-//   }
-
-//   type Link {
-//     id: ID!
-//     description: String!
-//     url: String!
-//   }
-// `
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
-
-// 1
-const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-  },
-  Mutation: {
-    // 2
-    post: (parent, args) => {
-
-    let idCount = links.length
-
-       const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
-    }
-  },
-}
-
-// 3
+const prisma = new PrismaClient()
+//  Context object that’s passed into all GraphQL resolvers is being initialized
 const server = new ApolloServer({
-  // typeDefs,
   typeDefs: fs.readFileSync(
     path.join(__dirname, 'schema.graphql'),
     'utf8'
   ),
   resolvers,
+  context: {
+    prisma,
+  }
 })
+
+
+// adding a new integer variable that simply serves as a very rudimentary way to generate unique IDs for newly created Link elements.
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    //accesses the prisma object via the context
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany()
+    },
+  },
+  Mutation: {
+    // implementation of the post resolver first creates a new link object,
+    // then adds it to the existing links list and finally returns the new link.
+     post: (parent, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      })
+      return newLink
+    },
+  },
+}
 
 server
   .listen()
