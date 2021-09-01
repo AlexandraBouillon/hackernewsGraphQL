bcfd4c2f132b327c8314241c0b89017e7afa6acf
alexandraBouillon <alexandra.bouillon@gmail.com>
2021-09-01 02:38:45 -0700

    updates

---
+++ src/index.js
@@ -0,0 +1,65 @@
+const { ApolloServer } = require('apollo-server');
+
+// Defining a new Link type that represents the links that can be posted to Hacker News.
+// Each Link has an id, a description, and a url.
+//  it’s not needed any more because the schema definition now lives in its own file(schema.graphql)
+// const typeDefs = `
+//   type Query {
+//     info: String!
+//     feed: [Link!]!
+//   }
+
+//   type Mutation {
+//     post(url: String!, description: String!): Link!
+//   }
+
+//   type Link {
+//     id: ID!
+//     description: String!
+//     url: String!
+//   }
+// `
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
+// 1
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Mutation: {
+    // 2
+    post: (parent, args) => {
+
+    let idCount = links.length
+
+       const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
+  },
+}
+
+// 3
+const server = new ApolloServer({
+  // typeDefs,
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8'
+  ),
+  resolvers,
+})
+
+server
+  .listen()
+  .then(({ url }) =>
+    console.log(`Server is running on ${url}`)
+  );
---
+++ src/script.js
@@ -0,0 +1,27 @@
+// Import the PrismaClient constructor from the @prisma/client node module
+const { PrismaClient } = require("@prisma/client")
+
+// Instantiate PrismaClient.
+const prisma = new PrismaClient()
+
+// Define an async function called main to send queries to the database. Write all queries inside this function.
+async function main() {
+//   const allLinks = await prisma.link.findMany()
+//   console.log(allLinks)
+const newLink = await prisma.link.create({
+    data: {
+      description: 'Fullstack tutorial for GraphQL',
+      url: 'www.howtographql.com',
+    },
+  })
+}
+
+// Call the main function.
+main()
+  .catch(e => {
+    throw e
+  })
+  // Close the database connections when the script terminates.
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
